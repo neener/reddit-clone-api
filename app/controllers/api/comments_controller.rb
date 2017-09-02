0e227f2aa4f2a3aa9b698f9defa11e58a4786b5b
Janine Rosen <janinemrosen@gmail.com>
2017-09-02 18:57:32 -0400

    adding comments controller and comments resource to routes

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,41 @@
+class API::CommentsController < ApplicationController
+	before_action :set_post
+	before_action :set_post_comment, only: [:show, :update, :destroy]
+
+	def index
+		render json: @post.comments, status: 200
+	end
+
+	def show
+		render json: @comment, status: 200
+	end
+
+	def create
+		@comment = @post.comments.create!(comment_params)
+		render json: @comment, status: 201
+	end
+
+	def update
+		@comment.update(comment_params)
+		render json: @comment, status: 200
+	end
+
+	def destroy
+		@comment.destroy
+		head :no_content
+	end
+
+	private
+
+		def comment_params
+			params.require(:comment).permit(:content)
+		end
+
+		def set_post
+			@post = Post.find(params[:post_id])
+		end
+
+		def set_post_comment
+			@comment = @post.comments.find_by!(id: params[:id]) if @post
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	namespace :api do
-		resources :posts, except: [:new, :edit]
+		resources :posts, except: [:new, :edit] do
+			resources :comments, except: [:new, :edit]
+		end
 	end
 end
