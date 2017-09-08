c71cc04fb723a9ea008abcb190dddfc046f991a4
Janine Rosen <janinemrosen@gmail.com>
2017-09-07 22:09:14 -0400

    clean?

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :link, :img_url, :upvote_count
+  attributes :id, :title, :link, :img_url, :upvote_count, :created_at
 
   has_many :comments
 end
